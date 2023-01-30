749d96585dbfe53a644ddc58c24ee1be63871602
Santi <santi.cocciardi@gmail.com>
2023-01-29 21:55:04 -0300

    deployed v1.3

--- server/dist/utils/checkBearerToken.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkBearerToken = void 0;
-const checkBearerToken = (token) => {
-    return token && token.startsWith('Bearer');
-};
-exports.checkBearerToken = checkBearerToken;
