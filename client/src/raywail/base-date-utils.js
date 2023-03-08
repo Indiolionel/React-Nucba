519861240503da987ddc770f351e36af26d8ae43
Santi <santi.cocciardi@gmail.com>
2023-03-08 00:12:08 -0300

    change url railway

--- client/src/raywail/base-date-utils.js
+++ client/src/raywail/base-date-utils.js
@@ -4,7 +4,6 @@ import swal from "sweetalert"
 
 const urlRailway = "https://plantasventas.up.railway.app/"
 
-
 // User Registration
 
 export const userRegistration = async (email, password, name) => {
